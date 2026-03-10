452a6e44f8119cbe11dd0f5d3753413f93ede800
matheo <matheo.leroy@ensae.fr>
2026-03-10 09:17:15 +0000

    change the title pf the descriptiopn of the api

--- app/api.py
+++ app/api.py
@@ -8,7 +8,7 @@ unknown_types = sio.get_untrusted_types(file="model.skops")
 model = sio.load("model.skops", trusted=unknown_types)
 
 app = FastAPI(
-    title="Prédiction de survie sur le Titanic",
+    title="Démonstration du modèle de prédiction de survie sur le Titanic",
     description="Application de prédiction de survie sur le Titanic 🚢 <br>Une version par API pour faciliter la réutilisation du modèle 🚀"
     + '<br><br><img src="https://media.vogue.fr/photos/5faac06d39c5194ff9752ec9/1:1/w_2404,h_2404,c_limit/076_CHL_126884.jpg" width="200">',
 )
